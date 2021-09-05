9a46d9ef44494fe76f60595254c849dcee97b7a0
강하은 <khe0124@gmail.com>
2021-09-05 23:55:11 +0900

    -

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,31 +1,16 @@
 import Axios from 'axios';
 import * as types from "../types"
 
+//인증코드 발급요청
 export async function getAuthCode(email: string) {
   return await Axios.get(`${process.env.VUE_APP_API_URL}api/reset-password?email=${email}`);
 }
-
+//안중코드 검증
 export async function postAuthCode(params: types.AuthCode) {
   return await Axios.post(`${process.env.VUE_APP_API_URL}api/reset-password`, params);
 }
-
+//비밀번호 변경
 export async function patchPassword(params: types.PassWordParams) {
   console.log(params);
   return await Axios.patch(`${process.env.VUE_APP_API_URL}api/reset-password`, params);
 }
-
-/* 
-인증코드 발급요청
-/api/reset-password?email=
-안중코드 검증
-post /api/reset-password
-비밀번호 변경
-patch /api/reset-password
-로그인
-post /api/login
-로그아웃
-post /api/logout
-회원정보조회
-get /api/user
-
-*/
