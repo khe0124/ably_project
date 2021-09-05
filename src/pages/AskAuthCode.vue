ffdcd4b396a91f08d43a70dcf982175b4ff4035c
강하은 <khe0124@gmail.com>
2021-09-05 23:51:04 +0900

    login vue

--- src/pages/AskAuthCode.vue
+++ src/pages/AskAuthCode.vue
@@ -4,7 +4,6 @@
         <form>
             <input type="text" v-model="email" placeholder="이메일을 입력하세요." class="input-md">
             <a @click="getPwAuth" class="btn">인증코드 받기</a>
-            <p>ably452@dummy.com</p>
             <p v-if="notMemeber">이메일이 없습니다.</p>
         </form>
     </div>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,8 +1,43 @@
 <template>
-    <p>login</p>
+    <div class="form-wrap">
+        <h2>로그인</h2>
+        <form>
+            <input type="text" v-model="email" placeholder="이메일" class="input-md">
+            <input type="password" v-model="password" placeholder="비밀번호" class="input-md">
+            <a @click="login" class="btn">로그인</a>
+            <a @click="changePassword">비밀번호 재설정</a>
+        </form>
+    </div>
 </template>
 <script lang="ts">
-import { Vue } from 'vue-class-component';
+import { reactive, toRefs } from 'vue';
+import { useRouter } from 'vue-router';
 
-export default class Login extends Vue {}
+export default {
+  name: 'login',
+
+  setup() {
+    const router = useRouter();
+    const data = reactive({
+      email: '',
+      password: '',
+    });
+
+    const changePassword = () => {
+      router.push({
+        name: 'AskAuthCode',
+      });
+    };
+
+    const login = () => {
+      console.log('login');
+    };
+
+    return {
+      ...toRefs(data),
+      changePassword,
+      login,
+    };
+  },
+};
 </script>
